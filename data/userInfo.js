f69a1eb33a2c2a9f851813569ee9425417a11d2c
WYseven <consolelog@126.com>
2016-11-14 14:42:58 +0800

    我的简历

---
+++ data/userInfo.js
@@ -0,0 +1,35 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "王允" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "web",
+    userQQ:  "1056104999",
+    userEmail:   "1056104999@qq.com",
+    motto:  "置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你置气和悲伤的源头都在于自己，你不听不闻不看，选择权在你", 
+    userPhone: 13141414820,
+    userOrignPlace:  "安徽省蚌埠市怀远县" ,
+    userSeatPlace:  "北京市",
+    userWeibo: "javascript@miaov.com",
+    
+}
+
